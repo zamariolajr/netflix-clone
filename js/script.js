2ea527788c60eb3c8109dd7035f078c1d0eddbf3
Jose Zamariola <zamariolajr@gmail.com>
2021-10-12 09:40:46 -0300

    Inserção de funcionalidade nos botões

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function play() {
+    window.open('https://www.youtube.com/watch?v=4PTYiM4B-LY', '_blank')
+}
+
+function info() {
+    window.open('https://www.themoviedb.org/tv/62852-billions', '_blank')
+}
